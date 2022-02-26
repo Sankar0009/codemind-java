5d5ada5b832fde23d0e2b3ff0a8b0b97c7c601c7
Sankar0009 <88707326+Sankar0009@users.noreply.github.com>
2022-02-26 15:10:40 +0530

    import java.util.*; class avg{     public static void main(String args[]){      Scanner sc=new Scanner(System.in);      int N=sc.nextInt();      int M=sc.nextInt();     double k=0.5000*(N+M);
         System.out.println(k+"000");
         
         }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class avg{
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     int N=sc.nextInt();
+     int M=sc.nextInt();
+    double k=0.5000*(N+M);
+    
+     
+     System.out.println(k+"000");
+     
+     }
+}
